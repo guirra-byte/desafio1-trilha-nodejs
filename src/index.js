6997859b17cfefca177c428cd088bdd9dcbb3799
Matheus Guirra Sousa <77081114+guirra-byte@users.noreply.github.com>
2021-12-10 12:34:37 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,3 @@
-/**const express = require('express');
-const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// const users = [];
-
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
-}
-
-app.post('/users', (request, response) => {
-  // Complete aqui
-});
-
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
-
-module.exports = app;*/
-
-// My Code
-const fs = require('fs')
 const express = require('express')
 const app = express()
 const { v4:uuidv4 } = require('uuid')
@@ -58,33 +14,27 @@ const customers = []
 function verifyUsernameExists(request, response, next){
   const { userName } = request.headers;
 
-  const verifyUserName = customers.find(user => user.userName === userName)
+  const verifyUserName = customers.some(user => user.userName === userName)
+
   if(!verifyUserName){
     return response.status(400).json({error: "UserName not found"})
   }
-  if(verifyUserName){
-    request.user = user
-    return next()
-    
-  }
+  
+
+  return next();
 
 
 }
 //Make Users
-app.post('/account', (request, response)=>{
-  const { name, userName } = request.body
+app.post('/user', (request, response)=>{
+  const { name, userName }  = request.body
+
   const userExists = customers.find(user => user.userName === userName)
 
   if(userExists){
     const error = "UserName already exists"
     return response.status(400).json({error: `${error}`})
   }
-
-  if(verifyUser){
-    const error = "Customer already exists"
-    return response.status(401).send(`${error}`)
-  }
-
   const customer = ({
     name, 
     userName, 
@@ -93,7 +43,7 @@ app.post('/account', (request, response)=>{
   })
   
   customers.push(customer)
-  return response.status(201).json(customer)
+  return response.status(201).send(customers)
 })
 
 //Take Users by your UserName
